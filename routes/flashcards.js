451957277dbb1a2eb98098016a93f1ba8bc13041
kgawerda <konradgawerda@gmail.com>
2023-12-04 12:31:37 +0100

    bypass user verification and refactor succed and fail cards

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -5,7 +5,7 @@ app = express();
 const mysql = require("mysql2");
 
 var connection = mysql.createConnection({
-  host: "172.18.96.1",
+  host: "172.22.0.1",
   port: "3306",
   user: "root",
   password: "",
@@ -24,6 +24,7 @@ function calculateDueDate(priority) {
     case -1:
       return date.toISOString().split("T")[0];
     case 0:
+      date.setDate(date.getDate() + 1);
       return date.toISOString().split("T")[0];
     case 1:
       date.setDate(date.getDate() + 3);
@@ -44,42 +45,44 @@ function calculateDueDate(priority) {
 }
 router.get("/get-new", (req, res) => {
   var last_item_id;
-  if (!req.user) res.send("user not logged in");
-  else {
-    connection.query(
-      "SELECT * from flashcards WHERE user_ref_id = " +
-        req.user.user_id +
-        " ORDER BY flashcard_id DESC LIMIT 1",
-      function (err, rows) {
-        if (err) next(err);
-        if (!rows.length) last_item_id = 0;
-        else last_item_id = rows[0].vocabulary_item_ref_id;
-        connection.query(
-          "SELECT * from vocabulary ORDER BY item_id ASC LIMIT " +
-            last_item_id +
-            ", 10",
-          function (err, rows) {
-            if (err) next(err);
-            for (let i = 0; i < rows.length; i++) {
-              connection.query(
-                "INSERT INTO flashcards (due_date, priority, vocabulary_item_ref_id, user_ref_id) VALUES ('" +
-                  currentDate +
-                  "', -1, " +
-                  rows[i].item_id +
-                  ", " +
-                  req.user.user_id +
-                  ")",
-                function (err, rows) {
-                  if (err) next(err);
-                }
-              );
-            }
-            res.send("items added");
+  //if (!req.user) res.send("user not logged in");
+  //else {
+  connection.query(
+    "SELECT * from flashcards WHERE user_ref_id = " +
+      //req.user.user_id +
+      "1" +
+      " ORDER BY flashcard_id DESC LIMIT 1",
+    function (err, rows) {
+      if (err) console.log(err);
+      if (!rows.length) last_item_id = 0;
+      else last_item_id = rows[0].vocabulary_item_ref_id;
+      connection.query(
+        "SELECT * from vocabulary ORDER BY item_id ASC LIMIT " +
+          last_item_id +
+          ", 10",
+        function (err, rows) {
+          if (err) console.log(err);
+          for (let i = 0; i < rows.length; i++) {
+            connection.query(
+              "INSERT INTO flashcards (due_date, priority, vocabulary_item_ref_id, user_ref_id) VALUES ('" +
+                currentDate +
+                "', -1, " +
+                rows[i].item_id +
+                ", " +
+                //req.user.user_id +
+                "1" +
+                ")",
+              function (err, rows) {
+                if (err) console.log(err);
+              }
+            );
           }
-        );
-      }
-    );
-  }
+          res.send("items added");
+        }
+      );
+    }
+  );
+  //}
 });
 
 router.get("/get-card/:id", (req, res) => {
@@ -96,115 +99,119 @@ router.get("/get-test-cards", (req, res) => {
   connection.query(
     "SELECT * from vocabulary ORDER BY item_id ASC LIMIT 10",
     function (err, rows) {
-      if (err) next(err);
+      if (err) console.log(err);
       else res.json(rows);
     }
   );
 });
 
 router.get("/get-due", (req, res) => {
-  if (!req.user) res.send("user not logged in");
-  else {
-    connection.query(
-      "SELECT * from flashcards WHERE user_ref_id = " +
-        req.user.user_id +
-        " AND due_date <= '" +
-        currentDate +
-        "'",
-      function (err, rows) {
-        if (err) next(err);
-        else if (rows.length) res.json(rows);
-        else res.send("no items due today");
-      }
-    );
-  }
+  // if (!req.user) res.send("user not logged in");
+  // else {
+  connection.query(
+    "SELECT * from flashcards WHERE user_ref_id = " +
+      //req.user.user_id +
+      "1" +
+      " AND due_date <= '" +
+      currentDate +
+      "'",
+    function (err, rows) {
+      if (err) console.log(err);
+      else if (rows.length) res.json(rows);
+      else res.send(false);
+    }
+  );
+  //}
 });
 
 router.get("/get-learning", (req, res) => {
-  if (!req.user) res.send("user not logged in");
-  else {
-    connection.query(
-      "SELECT * from flashcards WHERE user_ref_id = " +
-        req.user.user_id +
-        " AND priority = -1",
-      function (err, rows) {
-        if (err) next(err);
-        else if (rows.length) res.json(rows);
-        else res.send("no items in learning");
-      }
-    );
-  }
+  // if (!req.user) res.send("user not logged in");
+  // else {
+  connection.query(
+    "SELECT * from flashcards WHERE user_ref_id = " +
+      //req.user.user_id +
+      "1" +
+      " AND priority = -1",
+    function (err, rows) {
+      if (err) console.log(err);
+      else if (rows.length) res.json(rows);
+      else res.send(false);
+    }
+  );
+  // }
 });
 
-router.post("/succeed-card", (req, res) => {
-  if (!req.user) res.send("user not logged in");
-  else {
-    connection.query(
-      "SELECT * from flashcards WHERE user_ref_id = " +
-        req.user.user_id +
-        " AND flashcard_id = " +
-        req.body.flashcard_id +
-        "",
-      function (err, rows) {
-        if (err) next(err);
-        if (rows.length) {
-          var priority;
-          if (rows[0].priority === 5) priority = 5;
-          else priority = rows[0].priority + 1;
-          var dueDate = calculateDueDate(priority);
-          connection.query(
-            "UPDATE flashcards SET priority = " +
-              priority +
-              ", due_date ='" +
-              dueDate +
-              "' WHERE flashcard_id = " +
-              req.body.flashcard_id +
-              "",
-            function (err, res) {
-              if (err) next(err);
-              else res.send("card succeeded");
-            }
-          );
-        }
+router.post("/succeed-card/:flashcard_id", (req, res) => {
+  // if (!req.user) res.send("user not logged in");
+  // else {
+  connection.query(
+    "SELECT * from flashcards WHERE user_ref_id = " +
+      // req.user.user_id +
+      "1" +
+      " AND flashcard_id = " +
+      req.params.flashcard_id +
+      "",
+    function (err, rows) {
+      if (err) console.log(err);
+      if (rows.length) {
+        var priority;
+        if (rows[0].priority === 5) priority = 5;
+        else priority = rows[0].priority + 1;
+        var dueDate = calculateDueDate(priority);
+        connection.query(
+          "UPDATE flashcards SET priority = " +
+            priority +
+            ", due_date ='" +
+            dueDate +
+            "' WHERE flashcard_id = " +
+            req.params.flashcard_id +
+            "",
+          function (err, res) {
+            if (err) console.log(err);
+          }
+        );
       }
-    );
-  }
+    }
+  );
+  res.send("card succeeded");
+  //}
 });
 
-router.post("/fail-card", (req, res) => {
-  if (!req.user) res.send("user not logged in");
-  else {
-    connection.query(
-      "SELECT * from flashcards WHERE user_ref_id = " +
-        req.user.user_id +
-        " AND flashcard_id = " +
-        req.body.flashcard_id +
-        "",
-      function (err, rows) {
-        if (err) next(err);
-        if (rows.length) {
-          var priority;
-          if (rows[0].priority !== -1 && rows[0].priority !== 0)
-            priority = rows[0].priority - 1;
-          else priority = rows[0].priority;
-          var dueDate = calculateDueDate(priority);
-          connection.query(
-            "UPDATE flashcards SET priority = " +
-              priority +
-              ", due_date ='" +
-              dueDate +
-              "' WHERE flashcard_id = " +
-              req.body.flashcard_id +
-              "",
-            function (err, res) {
-              if (err) next(err);
-              else res.send("card succeeded");
-            }
-          );
-        }
+router.post("/fail-card/:flashcard_id", (req, res) => {
+  // if (!req.user) res.send("user not logged in");
+  // else {
+  connection.query(
+    "SELECT * from flashcards WHERE user_ref_id = " +
+      // req.user.user_id +
+      "1" +
+      " AND flashcard_id = " +
+      req.params.flashcard_id +
+      "",
+    function (err, rows) {
+      if (err) console.log(err);
+      if (rows.length) {
+        var priority;
+        if (rows[0].priority !== -1 && rows[0].priority !== 0)
+          priority = rows[0].priority - 1;
+        else priority = rows[0].priority;
+        var dueDate = calculateDueDate(priority);
+        connection.query(
+          "UPDATE flashcards SET priority = " +
+            priority +
+            ", due_date ='" +
+            dueDate +
+            "' WHERE flashcard_id = " +
+            req.params.flashcard_id +
+            "",
+          function (err, res) {
+            if (err) console.log(err);
+          }
+        );
       }
-    );
-  }
+    }
+  );
+  res.send("card failed");
+  //}
 });
 
 module.exports = router;
